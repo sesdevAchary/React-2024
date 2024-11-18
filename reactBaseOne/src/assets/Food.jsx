caa798af7a7b347295f9d969f025d30b585f62a1
sesdevAchary <sesdev.achary@gmail.com>
2024-11-18 18:11:27 +0000

    react initial  coverage

--- reactBaseOne/src/assets/Food.jsx
+++ reactBaseOne/src/assets/Food.jsx
@@ -1,4 +1,14 @@
 function Food(){
     const  Food1 = "Orange";
     const Food2="Banana"
+
+    return(
+        <ul>
+            <li>Orange</li>
+            <li>{Food2}</li> { /*js snippet always under the {} */}
+            <li>{Food</li>
+
+
+        </ul>
+    )
 }
