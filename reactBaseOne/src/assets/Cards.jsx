64ec827cc2b1c4635fb90423960966680e0496a5
sesdevAchary <sesdev.achary@gmail.com>
2024-11-20 15:19:22 +0000

    React mashup

--- reactBaseOne/src/assets/Cards.jsx
+++ reactBaseOne/src/assets/Cards.jsx
@@ -5,6 +5,7 @@ function cards(){
         <div className="Card">
            <img src="https://via.placeholder.com/150" alt="some picture"></img>
            <h2>Title Picture</h2>
+           <p>this is a normal pic to be inserted section </p>
         </div>
     )
 }
