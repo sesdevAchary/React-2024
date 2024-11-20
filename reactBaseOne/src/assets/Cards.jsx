b722211ebd508803a38d73fcf3f6cbf68383a901
sesdevAchary <sesdev.achary@gmail.com>
2024-11-20 15:18:47 +0000

    React mashup

--- reactBaseOne/src/assets/Cards.jsx
+++ reactBaseOne/src/assets/Cards.jsx
@@ -3,7 +3,8 @@
 function cards(){
     return(
         <div className="Card">
-           <img src="https://via.placeholder.com/150" alt="some picture"
+           <img src="https://via.placeholder.com/150" alt="some picture"></img>
+           <h2>Title Picture</h2>
         </div>
     )
 }
