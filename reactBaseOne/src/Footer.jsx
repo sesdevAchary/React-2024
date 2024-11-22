a327b56402750a688bcb3e2bad7b8b292d3a308d
sesdevAchary <sesdev.achary@gmail.com>
2024-11-22 17:51:36 +0000

    react survey

--- reactBaseOne/src/Footer.jsx
+++ reactBaseOne/src/Footer.jsx
@@ -2,6 +2,8 @@ function Footer(){
         return (
             <footer>
                 <p>&copy; This is all about the website</p>
+                <p>&copy; Footer of this is  the website</p>
+
             </footer>
         );
 };
