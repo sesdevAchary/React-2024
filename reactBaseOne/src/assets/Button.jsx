43c72e3721e0eeb5b9a22e11dbc692e3c6b654fe
sesdevAchary <sesdev.achary@gmail.com>
2024-11-23 07:39:28 +0000

    react subtle

--- reactBaseOne/src/assets/Button.jsx
+++ reactBaseOne/src/assets/Button.jsx
@@ -6,7 +6,7 @@ function Button (){
         border:"2px solid black",
 
     }
-    return( <button style={styles}>CLICK ME </button>);
+    return( <button style={styles}>CLICK ME </button>); {/* this is called internal styling , no problem for naming ambiguity*/}
 }
 
 export  default Button ;
