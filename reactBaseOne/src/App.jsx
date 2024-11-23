e002a278708c6be2988689623599a390747d81f0
sesdevAchary <sesdev.achary@gmail.com>
2024-11-23 17:49:49 +0000

    react subtle

--- reactBaseOne/src/App.jsx
+++ reactBaseOne/src/App.jsx
@@ -1,7 +1,6 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Button from './Button.jsx'
-import style from './style.css'
 // import Food from './Food.jsx'//
 
 function App() {
