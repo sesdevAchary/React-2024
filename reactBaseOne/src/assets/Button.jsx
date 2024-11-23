689de67b34b78166d43fb493eb96a8504b998c61
sesdevAchary <sesdev.achary@gmail.com>
2024-11-23 11:49:32 +0000

    react subtle

--- reactBaseOne/src/assets/Button.jsx
+++ reactBaseOne/src/assets/Button.jsx
@@ -2,13 +2,13 @@ function Button (){
 
 
     const style = {
-        backgroundColor: "azure", // within the JSX content we can't use any hyphen
+        backgroundColor: "azure", // within the JSX content we can't use any hyphen kind of statements.
       
       
         border:"2px solid black",
 
     }
-    return( <button style={styles}>CLICK ME </button>); {/* this is called internal styling , no problem for naming ambiguity*/}
+    return( <button style={style}>CLICK ME </button>); {/* this is called internal styling , no problem for naming ambiguity*/}
 }
 
 export  default Button ;
