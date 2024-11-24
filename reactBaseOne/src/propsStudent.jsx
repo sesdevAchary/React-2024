7c46f5b5c2cd46b0f65e0aca63e4ddd90ca773a3
sesdevAchary <sesdev.achary@gmail.com>
2024-11-24 06:33:00 +0000

    react soverign

---
+++ reactBaseOne/src/propsStudent.jsx
@@ -0,0 +1,8 @@
+function student(props){  {/*To accept props,student function needs a parameter,gonna be the jsObject */}
+return ( 
+    <div>
+        <p>Name: {props.name}</p>
+    </div>
+)
+}
+export default student;
