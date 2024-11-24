b905b46744e56ecd60e1db4c318510d960fdd871
sesdevAchary <sesdev.achary@gmail.com>
2024-11-24 06:58:19 +0000

    react native

--- reactBaseOne/src/propsStudent.jsx
+++ reactBaseOne/src/propsStudent.jsx
@@ -1,11 +1,13 @@
-function student(props){  {/*To accept props,student function needs a parameter,gonna be the jsObject */}
+function Student(props){  {/*To accept props,student function needs a parameter,gonna be the jsObject */}
 return ( 
     <div>
         <p>Name: {props.name}</p>
         <p>Age: {props.age}</p>
+        <p>Name: {props.isStudent ? " Nice ": "Not Nice"}</p>  {/*BOOLEANS need conditions , dont get directly displayed on Js Dom*/}
+
 
         </div>
 
 )
 }
-export default student;
+export default Student;
