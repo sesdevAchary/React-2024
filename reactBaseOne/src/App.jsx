98060fc1b0d8eb9f9a118eb43485d79b6db07273
sesdevAchary <sesdev.achary@gmail.com>
2024-11-24 06:38:05 +0000

    react soverign

--- reactBaseOne/src/App.jsx
+++ reactBaseOne/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Button from './Button.jsx'
+import student from './propsStudent.jsx'
 // import Food from './Food.jsx'//
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
     <Food/>
     <Footer/>
     <Button/>
+   
+   {/* including student component within the App component//App is the parent and student is the child component// */}
+    <student></student>
     </>
   );
 }
