5534b9c7a7c26fb5c006bb56832ada725ff15a41
sesdevAchary <sesdev.achary@gmail.com>
2024-11-25 13:33:14 +0000

    react soverign

--- reactBaseOne/src/renderingUserGreet.jsx
+++ reactBaseOne/src/renderingUserGreet.jsx
@@ -1,5 +1,5 @@
 function userGreeting(props){
      if( props.isLoggedIn){
-        return <h2>{props.userName}</h2>
+        return <h2>{props.userName}</h2> // "if true then this will be displayed"
      }
 }
