21cdf0fddc7b0122582021b97eb5b73715f12468
sesdevAchary <sesdev.achary@gmail.com>
2024-11-25 17:51:35 +0000

    react soverign

--- reactBaseOne/src/renderingUserGreet.jsx
+++ reactBaseOne/src/renderingUserGreet.jsx
@@ -1,7 +1,9 @@
+// conditional rendering = allows us to control what gets rendered in your applications based on some conditions 
+
+
 function userGreeting(props){
      return ( props.isLoggedIn   ?    <h2>welcome {props.userName}</h2> :
                                        <h2> else statement </h2>  
      );
        
      }
-}
