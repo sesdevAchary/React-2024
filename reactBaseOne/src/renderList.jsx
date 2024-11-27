5dd8bd0db36c0bbb2d1c8ee5ef22ea7403a757e0
sesdevAchary <sesdev.achary@gmail.com>
2024-11-27 18:04:26 +0000

    react base

--- reactBaseOne/src/renderList.jsx
+++ reactBaseOne/src/renderList.jsx
@@ -1,8 +1,10 @@
 function renderingList(){
-    const fruits = {apple,banan,Orange,coconut}
 
-    const listItems = fruits.map(fruit => <li>{fruits}</li>) {/*Goes through the fruits array and make it listed item*/}
-    
+ {/* first Example ------>
+     const fruits = {apple,banan,Orange,coconut}
+     const listItems = fruits.map(fruit => <li>{fruits}</li>);
+     return( <ol>listItems</ol>);
+*/}
 
-    return( <ol>listItems</ol>)
-}
+
+ }
