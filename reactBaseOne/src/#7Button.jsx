189701007f5438044cf28f0760a3c36e0d8000f8
sesdevAchary <sesdev.achary@gmail.com>
2024-11-28 15:39:54 +0000

    react base

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -1,4 +1,12 @@
 function Button (){
-    const handleCLick=()=> console.log("HELLO");
+
+
+   /* const handleCLick=()=> console.log("HELLO");
     return(<button onClick={handleCLick}> CLICK HERE</button>) // When clicked on the button it will render to the callBack onclick function //
+   */
+
+  const handleClick2=(name)=> console.log( `${name} stop clicking me `)
+  return( <button onclick={handleClick2("bro")}> click me </button>)
+
+
 }
