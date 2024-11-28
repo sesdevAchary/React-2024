7f889bfba45e64425d5c06f8a8e1d222d89abf17
sesdevAchary <sesdev.achary@gmail.com>
2024-11-28 15:42:02 +0000

    react base

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -1,12 +1,14 @@
 function Button (){
 
-
+// button click without a parameter
    /* const handleCLick=()=> console.log("HELLO");
     return(<button onClick={handleCLick}> CLICK HERE</button>) // When clicked on the button it will render to the callBack onclick function //
    */
 
+// button click having a parameter //
   const handleClick2=(name)=> console.log( `${name} stop clicking me `)
   return( <button onclick={handleClick2("bro")}> click me </button>)
 
 
 }
+export default Button ;
