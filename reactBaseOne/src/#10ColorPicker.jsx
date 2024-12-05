0cb0c25f45db5824eada7925ef169f6b81d295dc
sesdevAchary <sesdev.achary@gmail.com>
2024-12-05 12:04:47 +0000

    react base

--- reactBaseOne/src/#10ColorPicker.jsx
+++ reactBaseOne/src/#10ColorPicker.jsx
@@ -1,2 +1,12 @@
 import { useState } from "react"
 import react from 'react'
+
+function colorpicker(){
+    const[color,setColor]= useState("#FFFFFF");
+
+
+    // CREAING HTML ELEMENT WITHIN THE RETURN APP //
+    return(
+        <div className=""></div>
+    )
+}
