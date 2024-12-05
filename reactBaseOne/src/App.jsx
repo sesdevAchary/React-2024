a6cb223f164f38bc907607b74013cece70b92f2d
sesdevAchary <sesdev.achary@gmail.com>
2024-12-05 12:21:10 +0000

    react base

--- reactBaseOne/src/App.jsx
+++ reactBaseOne/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer.jsx'
 import Button from './#7Button.jsx'
 import student from './propsStudent.jsx'
 import userGreeting from './renderingUserGreet.jsx'
+import colorpicker from './#10ColorPicker.jsx'
 // import Food from './Food.jsx'//
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Food/>
     <Footer/>
     <Button/>
+    <colorpicker/>
    
    {/* including student component within the App component//App is the parent and student is the child component// */}
     <student name="Lnt ki Gotti" age={21} isStudent={fasle}/> {/* setting key value pair to student component this will also transfer to the props */}
