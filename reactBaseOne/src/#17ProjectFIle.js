9ffea2e68ad86e9f90265dad1f1e27f1f68cdb17
sesdevAchary <sesdev.achary@gmail.com>
2025-01-28 12:41:03 +0000

    react hook project 5

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -206,3 +206,22 @@ function button (){
       //return (<img onClick={(e)=>{handleClick5(e)} src = {imgUrl}></img>);//
  }
  
+
+
+ function App() {
+     const [click, setClick] = useState(0);
+     // using array destructuring here 
+     // to assign initial value 0
+     // to click and a reference to the function 
+     // that updates click to setClick
+     return (
+         <div>
+             <p>You clicked {click} times</p>
+ 
+             <button onClick={() => setClick(click + 1)}>
+                 Click me
+             </button>
+         </div>
+     );
+ }
+ 
