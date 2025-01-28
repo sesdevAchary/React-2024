23d2c14ef425891f633a2cfe3d6456a1e9c1edbb
sesdevAchary <sesdev.achary@gmail.com>
2025-01-28 12:34:03 +0000

    react subtle project 2

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -71,3 +71,15 @@ const Result = ({ term, secretNum }) => {
 }
 
 
+import React from 'react';
+import BoxContainer from './BoxContainer'
+
+function App() {
+return (
+	<div className="App">
+	<BoxContainer />
+	</div>
+);
+}
+
+
