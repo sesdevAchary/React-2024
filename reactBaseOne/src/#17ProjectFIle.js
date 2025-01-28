a957d9024d5c5faf15dbfd55c19daad5b4507d23
sesdevAchary <sesdev.achary@gmail.com>
2025-01-28 12:31:29 +0000

    react project 1

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -44,3 +44,30 @@ class App extends Component {
     }
 }
 export default App
+
+
+
+// Result.js
+
+import React from 'react'
+
+const Result = ({ term, secretNum }) => {
+	let result;
+	if (term) {
+		if (secretNum > term) {
+			result = 'Lower'
+		}
+		else if (secretNum < term) {
+			result = 'Higher'
+		}
+		else if (secretNum == term) {
+			result = 'Yippee, correct!'
+		}
+		else {
+			result = "Enter Valid Input"
+		}
+	}
+	return <h3>You Guessed: {result}</h3>
+}
+
+
