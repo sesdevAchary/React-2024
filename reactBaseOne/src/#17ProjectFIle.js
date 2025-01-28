ca6738a4191254952b1cf8305a7966c96ee951a1
sesdevAchary <sesdev.achary@gmail.com>
2025-01-28 12:37:32 +0000

    react hook project 3

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -143,3 +143,56 @@ render(){
 }
 }
 
+
+
+import React,{ Component } from 'react'
+
+class Box extends Component{
+constructor(props){
+	super(props)
+	this.handleChangeColor = this.handleChangeColor.bind(this)
+}
+
+// Handler callback
+handleChangeColor(){
+
+	// Call parent component cahngeColor method passing the
+	// color value of div
+	this.props.changeColor(this.props.color)
+}
+
+render(){
+
+	// Create a div component and assign the given 
+	// color value by BoxContainer component as its
+	// background color
+	return <div
+	
+	// Set click handler to the div and pass a callback
+	onClick={this.handleChangeColor} 
+	style={{backgroundColor:this.props.color, 
+				width:'13em', height:'13em'}} 
+	/>
+}
+}
+
+// Method return a random number from 0 to 255 
+const rgbValue = () => {
+    return Math.floor(Math.random() * 256)
+    }
+    
+    // Method generates an array of rgb colors
+    const generateColors = (num) => {
+    let colors = []
+    
+    for(let i=0; i<num; i++){
+        const red = rgbValue()
+        const blue = rgbValue()
+        const green = rgbValue()
+        colors.push(`rgb(${red},${blue},${green})`)
+    }
+    return colors
+    }
+    
+    export { rgbValue, generateColors }
+    
