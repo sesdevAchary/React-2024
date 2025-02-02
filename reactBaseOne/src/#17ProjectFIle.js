dfb76893df022a8e12ea699b6795b894c5cc345f
sesdevAchary <sesdev.achary@gmail.com>
2025-02-02 18:14:15 +0000

    fixing  the  styles portion

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -329,3 +329,19 @@ const Header = () => {
       </>
     );
   }
+
+
+  const Header = () => {
+    const myStyle = {
+      color: "white",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Sans-Serif"
+    };
+    return (
+      <>
+        <h1 style={myStyle}>Hello Style!</h1>
+        <p>Add a little style!</p>
+      </>
+    );
+  }
