843e5325284ad2ba5a01b9a4b171c67672eb2c0c
sesdevAchary <sesdev.achary@gmail.com>
2025-02-08 17:33:19 +0000

    todos list

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -175,9 +175,18 @@ const expensiveCalculation = (num) => {
   }
   return num;
 
-  <Todos todos={todos} addTodo={addTodo} />
-  <hr />
-  <div>
-    Count: {count}
-    <button onClick={increment}>+</button>
-  </div>
+ 
+  
+  
+const Todos = ({ todos, addTodo }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+      <button onClick={addTodo}>Add Todo</button>
+    </>
+  );
+};
