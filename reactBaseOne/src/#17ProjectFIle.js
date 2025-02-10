3f76b6b9583067057f19d7cd458c8e7e2bb02b89
sesdevAchary <sesdev.achary@gmail.com>
2025-02-10 17:56:42 +0000

    callback in reacts

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -185,3 +185,11 @@ function Garage() {
     {id: 3, brand: 'Audi'}
   ];
   return (
+    <>
+    <h1>Who lives in my garage?</h1>
+    <ul>
+      {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+    </ul>
+  </>
+);
+}
