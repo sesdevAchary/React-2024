6d2d878ef49c2afafff93ef7691407caa104fc9c
sesdevAchary <sesdev.achary@gmail.com>
2025-02-10 17:56:06 +0000

    callback in reacts

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -177,3 +177,11 @@ root.render(<Garage />);
 function Car(props) {
   return <li>I am a { props.brand }</li>;
 }
+
+function Garage() {
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
+  return (
