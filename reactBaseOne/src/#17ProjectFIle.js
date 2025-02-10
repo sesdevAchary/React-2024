fd780b0cd512a932e4f29cb8a562311e15ad82cb
sesdevAchary <sesdev.achary@gmail.com>
2025-02-10 17:59:28 +0000

    callback in reacts

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -193,3 +193,15 @@ function Garage() {
   </>
 );
 }
+
+function MyForm() {
+  return (
+    <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+    </form>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
