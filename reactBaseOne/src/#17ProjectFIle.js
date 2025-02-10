08afbadd4668038706abda79c0d268feda7db3af
sesdevAchary <sesdev.achary@gmail.com>
2025-02-10 17:54:34 +0000

    mapping onto

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -154,3 +154,20 @@ const Todos = ({ todos, addTodo }) => {
     parentComponent: ?React$Component<any, any>,
     callback: ?Function
   ): Lane 
+
+
+  function Car(props) {
+    return <li>I am a { props.brand }</li>;
+  }
+  
+  function Garage() {
+    const cars = ['Ford', 'BMW', 'Audi'];
+    return (
+      <>
+        <h1>Who lives in my garage?</h1>
+        <ul>
+          {cars.map((car) => <Car brand={car} />)}
+        </ul>
+      </>
+    );
+  }
