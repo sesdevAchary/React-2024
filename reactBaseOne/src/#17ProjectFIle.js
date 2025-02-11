dcf54acb1fb5ea1745b8da1e8895d9c022b02134
sesdevAchary <sesdev.achary@gmail.com>
2025-02-11 17:28:44 +0000

    callback in reacts

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -243,3 +243,16 @@ export default function App() {
   );
   const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+
+
+function Garage() {
+  const cars = ['Ford', 'BMW', 'Audi'];
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car brand={car} />)}
+      </ul>
+    </>
+  );
