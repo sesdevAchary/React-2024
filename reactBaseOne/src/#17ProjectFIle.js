15eee39e86bc1a200e40cec46f3e04574ca7c923
sesdevAchary <sesdev.achary@gmail.com>
2025-02-11 17:30:46 +0000

    callback in reacts

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -264,3 +264,15 @@ function Garage() {
   function MadeGoal() {
     return <h1>Goal!</h1>;
   }
+
+
+  function Goal(props) {
+    const isGoal = props.isGoal;
+    if (isGoal) {
+      return <MadeGoal/>;
+    }
+    return <MissedGoal/>;
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<Goal isGoal={false} />);
