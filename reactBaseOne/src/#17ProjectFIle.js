fb10cfba2558bab4feff6473ce277aadc30913f9
sesdevAchary <sesdev.achary@gmail.com>
2025-02-11 17:33:42 +0000

    callback in reacts

--- reactBaseOne/src/#17ProjectFIle.js
+++ reactBaseOne/src/#17ProjectFIle.js
@@ -286,3 +286,8 @@ function MyForm() {
       <label>Enter your name:
         <input type="text" />
       </label>
+      </form>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm />);
