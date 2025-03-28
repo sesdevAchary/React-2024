00c5d761f3de9394f48832ffb519da1eee3688cc
sesdevAchary <sesdev.achary@gmail.com>
2025-03-28 18:00:59 +0000

    counter higher function

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -27,3 +27,16 @@ const listItems= fruits.map(fruit => <li key={fruit.id}>{fruit.name}:&nbsp;
 
  }
 export default  renderingList;
+
+
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
