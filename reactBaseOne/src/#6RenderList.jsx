b4e1820d7fe61448934252efa28f6592bf0a0678
sesdevAchary <sesdev.achary@gmail.com>
2025-03-28 18:03:59 +0000

    adding the input effect

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -40,3 +40,18 @@ const Counter = () => {
     </div>
   );
 };
+const InputBox = () => {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={text} 
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Type something..."
+      />
+      <p>You typed: {text}</p>
+    </div>
+  );
+};
