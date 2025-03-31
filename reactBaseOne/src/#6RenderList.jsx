e98bb5e35c78cc541d9d1975d478253a5d76e6cc
sesdevAchary <sesdev.achary@gmail.com>
2025-03-31 17:23:46 +0000

    nav bar list

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -172,3 +172,17 @@ root.render(<Car />);
   </Route>
 </Routes>
 </BrowserRouter>
+
+<nav>
+<ul>
+  <li>
+    <Link to="/">Home</Link>
+  </li>
+  <li>
+    <Link to="/blogs">Blogs</Link>
+  </li>
+  <li>
+    <Link to="/contact">Contact</Link>
+  </li>
+</ul>
+</nav>
