8067a936b960dad929848e6ca15c5bf954b26df1
sesdevAchary <sesdev.achary@gmail.com>
2025-03-31 17:41:19 +0000

    set count function

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -209,3 +209,6 @@ export default NoPage;
 const App = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
