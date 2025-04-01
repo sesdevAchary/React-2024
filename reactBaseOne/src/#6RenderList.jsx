72b53537bb917c5a9b4b94c2ae5b1958ed321af8
sesdevAchary <sesdev.achary@gmail.com>
2025-04-01 03:48:57 +0000

    adding the extra todos

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -212,3 +212,16 @@ const App = () => {
   const increment = () => {
     setCount((c) => c + 1);
   };
+  const Todos = ({ todos }) => {
+    console.log("child render");
+    return (
+      <>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+      </>
+    );
+  };
+  
+  export default Todos;
