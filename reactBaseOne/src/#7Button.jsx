c03ac3c204c3858e324c647f4d46d32f1d6dd011
sesdevAchary <sesdev.achary@gmail.com>
2025-04-09 18:16:02 +0000

    completeing the code

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -122,3 +122,15 @@ useEffect(() => {
 const handleSubmit = (event) => {
   event.preventDefault();
   alert(`The name you entered was: ${name}`)
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+  )
