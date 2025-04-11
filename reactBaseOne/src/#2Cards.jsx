4b1b40507f0c82c6ac9a906a30b9efca8bd7de95
sesdevAchary <sesdev.achary@gmail.com>
2025-04-11 17:13:31 +0000

    using the namespace std

--- reactBaseOne/src/#2Cards.jsx
+++ reactBaseOne/src/#2Cards.jsx
@@ -66,3 +66,18 @@ function Counter() {
     </>
   );
 }
+
+import { useState, useEffect } from 'react';
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <p>Seconds: {seconds}</p>;
+}
