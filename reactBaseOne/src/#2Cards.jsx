8da2f5bbb720aca3284b4ca161213b50663533cd
sesdevAchary <sesdev.achary@gmail.com>
2025-04-11 17:12:44 +0000

    using the usestate hook

--- reactBaseOne/src/#2Cards.jsx
+++ reactBaseOne/src/#2Cards.jsx
@@ -54,3 +54,15 @@ useEffect(() => {
   
   // Usage: <Welcome name="Alice" />
   
+  import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  );
+}
