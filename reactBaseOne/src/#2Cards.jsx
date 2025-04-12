c25bf87312ad66eee4057d14f4c9f55179ebd382
sesdevAchary <sesdev.achary@gmail.com>
2025-04-12 17:09:42 +0000

    toggle component added

--- reactBaseOne/src/#2Cards.jsx
+++ reactBaseOne/src/#2Cards.jsx
@@ -153,3 +153,17 @@ function Counter() {
     </div>
   );
 }
+
+import { useState } from 'react';
+
+function ToggleText() {
+  const [visible, setVisible] = useState(true);
+  return (
+    <div>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? 'Hide' : 'Show'} Text
+      </button>
+      {visible && <p>This is some text!</p>}
+    </div>
+  );
+}
