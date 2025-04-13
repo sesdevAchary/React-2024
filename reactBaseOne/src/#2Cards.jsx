5f28f7d4901462259578d0336baaffc71c36552f
sesdevAchary <sesdev.achary@gmail.com>
2025-04-13 13:04:16 +0000

    fetching the data through use effect

--- reactBaseOne/src/#2Cards.jsx
+++ reactBaseOne/src/#2Cards.jsx
@@ -338,3 +338,25 @@ function Card({ title, children }) {
 }
 
 export default Card;
+
+import React, { useState, useEffect } from 'react';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default Users;
