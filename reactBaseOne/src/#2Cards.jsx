efd4b5177f753964d9ea88800f80c74000fc1e64
sesdevAchary <sesdev.achary@gmail.com>
2025-04-13 13:03:12 +0000

    custom hook added

--- reactBaseOne/src/#2Cards.jsx
+++ reactBaseOne/src/#2Cards.jsx
@@ -310,3 +310,18 @@ function ItemList({ items }) {
 }
 
 export default ItemList;
+import { useState, useEffect } from 'react';
+
+function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return width;
+}
+
+export default useWindowWidth;
