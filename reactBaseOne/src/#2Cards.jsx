ff7092bb9ed35ea53522d76148cf5de73f6d1fc6
sesdevAchary <sesdev.achary@gmail.com>
2025-04-13 13:00:18 +0000

    Using useState for Counter

--- reactBaseOne/src/#2Cards.jsx
+++ reactBaseOne/src/#2Cards.jsx
@@ -270,3 +270,18 @@ function Welcome() {
 }
 
 export default Welcome;
+
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+}
+
+export default Counter;
