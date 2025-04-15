698bca8f9ebc30217419aa03147cf6172ceade66
sesdevAchary <sesdev.achary@gmail.com>
2025-04-15 18:13:31 +0000

    using the context api

--- reactBaseOne/src/#4PropsStudent.jsx
+++ reactBaseOne/src/#4PropsStudent.jsx
@@ -338,3 +338,21 @@ function Footer() {
 }
 
 export default Page;
+import { createContext, useContext } from "react";
+
+const ThemeContext = createContext("light");
+
+function ThemedComponent() {
+  const theme = useContext(ThemeContext);
+  return <div>The current theme is {theme}</div>;
+}
+
+function App() {
+  return (
+    <ThemeContext.Provider value="dark">
+      <ThemedComponent />
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
