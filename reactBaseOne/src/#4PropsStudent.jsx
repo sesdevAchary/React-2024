fd862881872f9bb091892e991b011ef3f4dba1ab
sesdevAchary <sesdev.achary@gmail.com>
2025-04-15 18:10:07 +0000

    using the useState hook

--- reactBaseOne/src/#4PropsStudent.jsx
+++ reactBaseOne/src/#4PropsStudent.jsx
@@ -243,3 +243,17 @@ const InfiniteScroll = () => {
     
     export default Greeting;
     
+    import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
