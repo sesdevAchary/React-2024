a574fecf4c046adadbcd6482472daaad02230b4c
sesdevAchary <sesdev.achary@gmail.com>
2025-04-15 18:10:39 +0000

    now with useeffect

--- reactBaseOne/src/#4PropsStudent.jsx
+++ reactBaseOne/src/#4PropsStudent.jsx
@@ -257,3 +257,21 @@ function Counter() {
 }
 
 export default Counter;
+
+import { useEffect, useState } from "react";
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <h2>Time: {seconds} seconds</h2>;
+}
+
+export default Timer;
