80b441195f04b5bb02694cdbf76963049e233ddd
sesdevAchary <sesdev.achary@gmail.com>
2025-04-18 17:26:29 +0000

    adding the button

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -140,4 +140,15 @@ const handleSubmit = (event) => {
   function HelloWorld() {
     return <h1>Hello, World!</h1>;
   }
-  
+  import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
