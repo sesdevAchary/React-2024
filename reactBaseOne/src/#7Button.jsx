d7432343c28f712d73db35bdd51ce25e1ef18b54
sesdevAchary <sesdev.achary@gmail.com>
2025-04-19 16:47:10 +0000

    click counter

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -2,3 +2,14 @@ function App() {
   return <h1>Hello, World!</h1>;
 }
 export default App;
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      You clicked {count} times
+    </button>
+  );
+}
+export default App;
