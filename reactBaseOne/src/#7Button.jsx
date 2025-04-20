52a4718e1b93040ab3989841e89a2f50c48d702e
sesdevAchary <sesdev.achary@gmail.com>
2025-04-20 17:59:06 +0000

    second timer useeffect

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -208,3 +208,15 @@ function DarkMode() {
     </div>
   );
 }
+import { useEffect, useState } from 'react';
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => setSeconds(s => s + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <p>Timer: {seconds}s</p>;
+}
