65efecc312932a9c1ba40a900d2582f0918fac19
sesdevAchary <sesdev.achary@gmail.com>
2025-04-21 17:12:56 +0000

    simple text effect

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -6,3 +6,10 @@ function changeText() {
   document.getElementById("demo").innerHTML = "You clicked the button!";
 }
 </script>
+<script>
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(5, 3)); // Output: 8
+</script>
