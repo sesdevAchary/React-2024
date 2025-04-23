cb8da148ac5668aef7937606d017f142ac259d33
sesdevAchary <sesdev.achary@gmail.com>
2025-04-23 17:33:13 +0000

    checking a prime number

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -135,3 +135,10 @@ function copyToClipboard(text) {
     console.log("Copied to clipboard!");
   });
 }
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
