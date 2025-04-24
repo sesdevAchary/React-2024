cf3a85adf45d0540d03a37c4f7259a74f530e5d3
sesdevAchary <sesdev.achary@gmail.com>
2025-04-24 18:03:44 +0000

    promise functio added

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -201,3 +201,9 @@ function isArray(value) {
 }
 
 console.log(isArray([1, 2, 3])); // true
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Usage
+sleep(1000).then(() => console.log("1 second passed"));
