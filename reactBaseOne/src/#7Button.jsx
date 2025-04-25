49287140d1d85d32aa737276fd78c67e0212197d
sesdevAchary <sesdev.achary@gmail.com>
2025-04-25 17:11:59 +0000

    fetching the api

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -219,3 +219,7 @@ function addTask() {
   document.getElementById("list").appendChild(li);
 }
 </script>
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
