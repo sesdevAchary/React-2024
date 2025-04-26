5d56df454a13403c631ed2a0e387d24ef908be5a
sesdevAchary <sesdev.achary@gmail.com>
2025-04-26 17:49:16 +0000

    Lru cache system

--- reactBaseOne/src/#7Button.jsx
+++ reactBaseOne/src/#7Button.jsx
@@ -353,3 +353,21 @@ class EventEmitter {
   }
 }
 
+class LRUCache {
+  constructor(limit = 5) {
+    this.cache = new Map();
+    this.limit = limit;
+  }
+  get(key) {
+    if (!this.cache.has(key)) return null;
+    const value = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  }
+  put(key, value) {
+    if (this.cache.has(key)) this.cache.delete(key);
+    else if (this.cache.size === this.limit) this.cache.delete(this.cache.keys().next().value);
+    this.cache.set(key, value);
+  }
+}
