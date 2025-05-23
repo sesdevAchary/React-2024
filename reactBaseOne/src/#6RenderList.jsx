6a8d9348ac96ba065fb6183747d14fbd5cdba465
sesdevAchary <sesdev.achary@gmail.com>
2025-05-23 17:15:03 +0000

    scrolling element

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -1,3 +1,6 @@
 function getQueryParams() {
   return Object.fromEntries(new URLSearchParams(window.location.search));
 }
+function scrollToElement(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
