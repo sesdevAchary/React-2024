f67d1712a14b3540d43747a962106c253418121f
sesdevAchary <sesdev.achary@gmail.com>
2025-05-23 17:16:06 +0000

    checkin for a number is prime

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -7,3 +7,10 @@ function scrollToElement(id) {
 function shuffleArray(arr) {
   return arr.sort(() => Math.random() - 0.5);
 }
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
