d2f2938c89cc4614b7eed489ce63b3e824743cd1
sesdevAchary <sesdev.achary@gmail.com>
2025-05-25 17:24:57 +0000

    working with custom dom carry engine

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -205,3 +205,6 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
   }
   const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
 const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+function $(selector, root = document) {
+  return [...root.querySelectorAll(selector)];
+}
