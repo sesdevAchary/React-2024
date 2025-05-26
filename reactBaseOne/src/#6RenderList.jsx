d8e11f0bb2c570b62c10f79bf24041e101319dd7
sesdevAchary <sesdev.achary@gmail.com>
2025-05-26 16:54:32 +0000

    shifting the lru search

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -265,3 +265,25 @@ function binarySearch(arr, target) {
   }
   return -1;
 }
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.map = new Map();
+  }
+
+  get(key) {
+    if (!this.map.has(key)) return -1;
+    const value = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, value);
+    return value;
+  }
+
+  put(key, value) {
+    if (this.map.has(key)) this.map.delete(key);
+    this.map.set(key, value);
+    if (this.map.size > this.capacity) {
+      this.map.delete(this.map.keys().next().value);
+    }
+  }
+}
