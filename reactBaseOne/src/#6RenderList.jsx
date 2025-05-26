853af400ba45d885d8e8a11c0b163cf3b0aa2ff6
sesdevAchary <sesdev.achary@gmail.com>
2025-05-26 16:52:37 +0000

    going with binary search

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -255,3 +255,13 @@ function curry(fn) {
       : (...next) => curried(...args, ...next);
   };
 }
+function binarySearch(arr, target) {
+  let [left, right] = [0, arr.length - 1];
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+}
