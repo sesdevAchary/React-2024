974db246c89ff094743ce39f12f8e0dc6db1687d
sesdevAchary <sesdev.achary@gmail.com>
2025-05-27 17:17:43 +0000

    checking for prime fun

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -364,3 +364,10 @@ function copyToClipboard(text) {
     alert("Copied to clipboard!");
   });
 }
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
