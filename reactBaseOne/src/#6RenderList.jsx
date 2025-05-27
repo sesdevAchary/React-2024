2cb2ecf4e08ce2cec4624e85176a290a8b3c7d67
sesdevAchary <sesdev.achary@gmail.com>
2025-05-27 17:25:09 +0000

    checkin for real object

--- reactBaseOne/src/#6RenderList.jsx
+++ reactBaseOne/src/#6RenderList.jsx
@@ -398,3 +398,6 @@ function getRandomHexColor() {
 function getRandomHexColor() {
   return '#' + Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0');
 }
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
