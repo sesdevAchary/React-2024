12b51f940c9f5145b09a01559321abeb7bad74b6
sesdevAchary <sesdev.achary@gmail.com>
2025-05-30 13:19:21 +0000

    calaculating the time complexity of bubble sort

--- DSA/Main.java
+++ DSA/Main.java
@@ -35,3 +35,16 @@ for (int i = 0; i < n/2; i++) {
     }
 }        // Time Complexity = O(n * m)
 
+boolean swapped;
+for (int i = 0; i < n - 1; i++) {
+    swapped = false;
+    for (int j = 0; j < n - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+            swapped = true;
+        }
+    }
+    if (!swapped) break; // bubble sort O(n^2)
+}
