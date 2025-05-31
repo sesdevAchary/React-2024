82cd1f6ac35fa9d074c01101b2bb5baadf6c3db6
sesdevAchary <sesdev.achary@gmail.com>
2025-05-31 16:51:20 +0000

    checking for a palindrome

--- reactBaseOne/src/#10ColorPicker.jsx
+++ reactBaseOne/src/#10ColorPicker.jsx
@@ -64,3 +64,17 @@ public class Fibonacci {
         }
     }
 }
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, rev = 0, original = num;
+        while (num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num /= 10;
+        }
+        if (original == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
