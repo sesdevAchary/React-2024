359d638e58884d92077e3416c789264d9057f051
sesdevAchary <sesdev.achary@gmail.com>
2025-05-31 16:50:31 +0000

    even factorial function added

--- reactBaseOne/src/#10ColorPicker.jsx
+++ reactBaseOne/src/#10ColorPicker.jsx
@@ -43,3 +43,12 @@ public class EvenOdd {
             System.out.println("Odd");
     }
 }
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5, fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial: " + fact);
+    }
+}
