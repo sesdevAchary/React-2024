846a31d66ca10d09da7af54eeba827ae8d5a1e95
sesdevAchary <sesdev.achary@gmail.com>
2025-05-31 16:53:15 +0000

    printing the largest one

--- reactBaseOne/src/#10ColorPicker.jsx
+++ reactBaseOne/src/#10ColorPicker.jsx
@@ -98,3 +98,11 @@ public class PrimeCheck {
         System.out.println(isPrime ? "Prime" : "Not Prime");
     }
 }
+
+public class Largest {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 15;
+        int max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println("Largest: " + max);
+    }
+}
