af2937c007c9578c06e041521c0dfaafeb1e22e4
sesdevAchary <sesdev.achary@gmail.com>
2025-05-31 16:58:40 +0000

    array sum checker

--- reactBaseOne/src/#10ColorPicker.jsx
+++ reactBaseOne/src/#10ColorPicker.jsx
@@ -115,3 +115,13 @@ public class Swap {
         System.out.println("a = " + a + ", b = " + b);
     }
 }
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
