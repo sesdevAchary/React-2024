6066b93beb9bba109b8f26f0749fe8932f4390cf
sesdevAchary <sesdev.achary@gmail.com>
2025-05-31 16:52:09 +0000

    checking for prime number

--- reactBaseOne/src/#10ColorPicker.jsx
+++ reactBaseOne/src/#10ColorPicker.jsx
@@ -85,3 +85,16 @@ public class ReverseString {
         System.out.println("Reversed: " + reversed);
     }
 }
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
