4abb595f435171767b54c0d8dac6ed8783377345
sesdevAchary <sesdev.achary@gmail.com>
2025-06-06 13:54:11 +0000

    cleared with the mistakes

--- DSA/methods.java
+++ DSA/methods.java
@@ -1,9 +1,12 @@
-public int addnum(int a,int b){
+public class methods { 
+    public static int addnum(int a,int b){
     int sum= a+b;
     return sum;
 }
 
 public static void main ( String [] args){
     int a =5; int b=6;
-    addnum();
+    int result = addnum(a, b);        // store the returned value
+        System.out.println(result); 
+}
 }
