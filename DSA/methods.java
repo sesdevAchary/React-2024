e12294cd940ef7c17c96f4d02d13af4c8f8a44eb
sesdevAchary <sesdev.achary@gmail.com>
2025-06-06 18:25:19 +0000

    halfway code manipulation

--- DSA/methods.java
+++ DSA/methods.java
@@ -1,12 +1,34 @@
-public class methods { 
-    public static int addnum(int a,int b){
-    int sum= a+b;
-    return sum;
-}
+// public class methods { 
+//     public static int addnum(int a,int b){
+//     int sum= a+b;
+//     return sum;
+// }
 
-public static void main ( String [] args){
-    int a =5; int b=6;
-    int result = addnum(a, b);        // store the returned value
-        System.out.println(result); 
-}
+// public static void main ( String [] args){
+//     int a =5; int b=6;
+//     int result = addnum(a, b);        // store the returned value
+//         System.out.println(result); 
+// }
+// }
+
+
+
+class calculator {
+       public int sub ( int a, int b)
+       {
+        return a-b;
+       }
+
+       static void (){
+        system.out.println("welcoome here ")
+       }
+       class method{
+        public static void main (String[] args)
+       {
+        class calculator
+
+       }
+
+       }
+       
 }
