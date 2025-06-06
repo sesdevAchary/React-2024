b074d00007b9b8e17662ef5ba08401b9c76cda64
sesdevAchary <sesdev.achary@gmail.com>
2025-06-06 13:27:29 +0000

    adding the nums using methods

--- DSA/methods.java
+++ DSA/methods.java
@@ -1,4 +1,9 @@
-public int addnum(a,b){
+public int addnum(int a,int b){
     int sum= a+b;
     return sum;
+}
+
+public static void main ( String [] args){
+    int a =5; int b=6;
+    addnum();
 }
