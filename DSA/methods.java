43b026f1f9ddac6210afa21f7b87d4942faa3c31
sesdevAchary <sesdev.achary@gmail.com>
2025-06-06 18:31:30 +0000

    instant method added

--- DSA/methods.java
+++ DSA/methods.java
@@ -34,3 +34,36 @@ class calculator {
        }
        
 }
+
+
+
+
+// Java program demonstrates how to call a method
+class Add {
+    int s = 0;
+    
+    // Method 
+    public int addTwoInt(int a, int b) {
+        // Perfrom sum of two numbers given as argument
+        s = a + b;
+        
+        // Return the sum of two numbers
+        return s;
+    }
+}
+
+class Geeks
+{
+    public static void main(String[] args) {
+        
+        // Creating an object of Add class
+        Add a = new Add();
+        
+        // Calling method using object and storing the return 
+        // Value in res variable of int type
+        int res = a.addTwoInt(1, 2);
+        
+        // Printing the output
+        System.out.println("Sum: " + res);  
+    }
+}
