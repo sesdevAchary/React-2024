5481d1859fb603a702f12982755c99842f0efc81
sesdevAchary <sesdev.achary@gmail.com>
2025-06-06 18:32:30 +0000

    calling through consttucter

--- DSA/methods.java
+++ DSA/methods.java
@@ -67,3 +67,57 @@ class Geeks
         System.out.println("Sum: " + res);  
     }
 }
+
+// Java Program to Illustrate Method Calling
+import java.io.*;
+
+// Helper class
+class Test {
+
+    public static int i = 0;
+
+    // Constructor to count objects
+    Test() {
+        i++;
+    }
+
+    // Static method to get the 
+    // number of objects created
+    public static int get() {
+        return i;
+    }
+
+    // Instance method m1 calling 
+    // another method m2
+    public int m1() {
+        System.out.println("Inside the method m1");
+        this.m2();  // Calling m2 method
+        return 1;
+    }
+
+    // Method m2 that prints a message
+    public void m2() {
+        System.out.println("In method m2");
+    }
+}
+
+// Main class
+class Geeks 
+{
+    // Main driver method
+    public static void main(String[] args) {
+
+        // Creating object of Test class
+        Test obj = new Test();
+
+        // Calling m1 method using the class object
+        int i = obj.m1();
+        System.out.println("Control returned after m1: " + i);
+
+        // Calling the get method directly using the class name
+        int o = Test.get();
+        
+        // Printing the value
+        System.out.println("No of instances created: " + o);
+    }
+}
