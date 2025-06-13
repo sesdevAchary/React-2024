da4ea5f599c19b4595b9e78dab4c554d4b77257e
sesdevAchary <sesdev.achary@gmail.com>
2025-06-13 17:03:39 +0000

    perfect sine value added

--- DSA/Main.java
+++ DSA/Main.java
@@ -271,22 +271,29 @@
 
 
 
-public static boolean pallindrome(String s){
-  int left =0; int right = s.length() -1;
+// public static boolean palindrome(String s){
+//   int left =0; int right = s.length() -1;
 
-  while(left<right){
-    if ( s.charAt(left++) != s.charAt(right--))
-    return false;
+//   while(left<right){
+//     if ( s.charAt(left++) != s.charAt(right--))
+//     return false;
    
-  }
-  return true;
+//   }
+//   return true;
   
-}
-public static void main (String[] args){
-  System.out.println(isPalindrome("madam"));  
-        System.out.println(isPalindrome("java"));   
-}
+// }
+// public static void main (String[] args){
+//   System.out.println(palindrome("madam"));  
+//         System.out.println(palindrome("java"));   
+// }
+
+
 
 }
+
+
+
+
+
 
 
