520cb997d551571b183f18fad1c622c802d973d2
sesdevAchary <sesdev.achary@gmail.com>
2025-06-14 18:00:12 +0000

    shuffling the array

--- DSA/Main.java
+++ DSA/Main.java
@@ -376,3 +376,9 @@ function debounce(func, delay) {
   };
 }
 const flat = arr => arr.reduce((acc, val) => acc.concat(val), []);
+function shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
