e88cce7ecfe7013ef369b0ef80840282a6923965
sesdevAchary <sesdev.achary@gmail.com>
2025-06-15 12:04:05 +0000

    defining the versatile element

---
+++ DSA/sorting.java
@@ -0,0 +1,18 @@
+public class sorting{
+    public int linearSearch(int array, int target)
+    int index = -1;
+    for(int i=0;i<array.length;i++){
+        if(array[i] == target){
+        return target;
+        index=0;
+    }
+    }
+    public static void main ( Str[] args){
+        scanner sc = new scanner (System.in);
+        S
+        
+        System.out.println("enter the array ");
+
+
+    }
+}
