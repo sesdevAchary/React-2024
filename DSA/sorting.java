ecc8b52a560f2912bb25b525f18d03423ebd4b82
sesdevAchary <sesdev.achary@gmail.com>
2025-06-16 12:07:49 +0000

    counting the numero value

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -1,29 +1,40 @@
 import java.util.*;
 
 public class sorting{
-    public  static int linearSearch(int[] array, int target){
-    for(int i=0;i<array.length;i++){
-        if(array[i] == target){
-        return i;
-    }
-    }
-    return -1 ; 
-    }
-    public static void main ( String[] args){
-        Scanner sc = new Scanner (System.in);
+//     public  static int linearSearch(int[] array, int target){
+//     for(int i=0;i<array.length;i++){
+//         if(array[i] == target){
+//         return i;
+//     }
+//     }
+//     return -1 ; 
+//     }
+//     public static void main ( String[] args){
+//         Scanner sc = new Scanner (System.in);
 
-        System.out.println("enter the array size you want-> ");
-        int size = sc.nextInt();
+//         System.out.println("enter the array size you want-> ");
+//         int size = sc.nextInt();
 
-        System.out.println(" enter the list-> ");
-        int array[] = new int[size];
-        for(int j=0;j<size;j++){
-        array[j]=sc.nextInt();
-        }
+//         System.out.println(" enter the list-> ");
+//         int array[] = new int[size];
+//         for(int j=0;j<size;j++){
+//         array[j]=sc.nextInt();
+//         }
         
-        System.out.println("enter the element you want to search-> ");
-        int target= sc.nextInt();
-   sc.close();
+//         System.out.println("enter the element you want to search-> ");
+//         int target= sc.nextInt();
+//    sc.close();
 
+//     }
+
+
+public static int  bianrySearch( int[] array,int target ){
+    int low = 0;
+    int high = array.length-1;
+
+    while(low<high){
+        int mid = low+(low++high)/2;
     }
+
+}
 }
