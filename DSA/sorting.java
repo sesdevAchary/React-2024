58e8c11d50848222b261f2bec597c9ccf5827aad
sesdevAchary <sesdev.achary@gmail.com>
2025-06-16 12:17:33 +0000

    getting the wrong output

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -1,6 +1,6 @@
-import java.util.*;
+// import java.util.*;
 
-public class sorting{
+// public class sorting{
 //     public  static int linearSearch(int[] array, int target){
 //     for(int i=0;i<array.length;i++){
 //         if(array[i] == target){
@@ -27,14 +27,43 @@ public class sorting{
 
 //     }
 
+import java.util.*;
 
+public class sorting{
 public static int  bianrySearch( int[] array,int target ){
     int low = 0;
     int high = array.length-1;
 
     while(low<high){
         int mid = low+(low++high)/2;
-    }
 
+        if(array[mid]== target){
+            return mid;
+        }
+        if(target<array[mid])
+        high=mid-1;
+
+        if(target>array[mid])
+        low=mid+1;
+    }
+    return  -1;
+
+}
+
+ public static void main ( String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("enter the array size you want-> ");
+        int size = sc.nextInt();
+
+        System.out.println(" enter the list-> ");
+        int array[] = new int[size];
+        for(int j=0;j<size;j++){
+        array[j]=sc.nextInt();
+        }
+        
+        System.out.println("enter the element you want to search-> ");
+        int target= sc.nextInt();
+   sc.close();
 }
 }
