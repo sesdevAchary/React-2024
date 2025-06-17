16a775419a1bc184105a79766b9a1f0e332f36a3
sesdevAchary <sesdev.achary@gmail.com>
2025-06-17 18:01:47 +0000

    continuing with the insertion  sort

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -175,3 +175,12 @@ def selection_sort(arr):
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
