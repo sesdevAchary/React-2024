5ca0c831f68cd8c775076aff687659bd867cf7ac
sesdevAchary <sesdev.achary@gmail.com>
2025-06-17 18:01:25 +0000

    continuing with the selection  sort

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -167,3 +167,11 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
