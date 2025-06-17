97c853e14f4e93270c79e37170cb6ee9921d0b54
sesdevAchary <sesdev.achary@gmail.com>
2025-06-17 18:00:50 +0000

    continuing with the bubble sort

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -160,4 +160,10 @@ public class sorting {
 
 
 
-c
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
