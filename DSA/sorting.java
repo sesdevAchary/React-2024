2a0a49ce6eded69ea87428a68b3f4e5d69835277
sesdevAchary <sesdev.achary@gmail.com>
2025-06-18 18:20:58 +0000

    preferring with the radix  sorting technique

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -276,3 +276,23 @@ def tim_sort(arr):
         gap //= 2
     return arr
 
+def radix_sort(arr):
+    RADIX = 10
+    max_length = False
+    tmp, placement = -1, 1
+    while not max_length:
+        max_length = True
+        buckets = [[] for _ in range(RADIX)]
+        for i in arr:
+            tmp = i // placement
+            buckets[tmp % RADIX].append(i)
+            if max_length and tmp > 0:
+                max_length = False
+        a = 0
+        for b in buckets:
+            for i in b:
+                arr[a] = i
+                a += 1
+        placement *= RADIX
+    return arr
+`
