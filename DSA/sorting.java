68652541acacf72b2555e9105fdd112385457977
sesdevAchary <sesdev.achary@gmail.com>
2025-06-18 18:20:36 +0000

    preferring with the shell sorting technique

--- DSA/sorting.java
+++ DSA/sorting.java
@@ -262,3 +262,17 @@ public static void divide(int arr[], int si, int ei) {
 
 def tim_sort(arr):
     return sorted(arr)
+
+    def shell_sort(arr):
+    gap = len(arr) // 2
+    while gap > 0:
+        for i in range(gap, len(arr)):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
