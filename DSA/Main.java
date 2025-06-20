536abdb825f676a66540ca44b5417c06de7642c5
sesdevAchary <sesdev.achary@gmail.com>
2025-06-20 18:15:37 +0000

    encapsulating the  methods

--- DSA/Main.java
+++ DSA/Main.java
@@ -603,3 +603,14 @@ class Cat extends Animal {
         System.out.println("Cat meows");
     }
 }
+class Bank {
+    private int balance = 1000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        if (amount > 0) balance += amount;
+    }
+}
