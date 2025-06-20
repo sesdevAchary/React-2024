014e9e292db909e2b81b762ef8dec8c08e2ee6a3
sesdevAchary <sesdev.achary@gmail.com>
2025-06-20 18:14:41 +0000

    inheritance effect named

--- DSA/Main.java
+++ DSA/Main.java
@@ -580,3 +580,14 @@ class Rectangle {
         System.out.println("Area: " + (length * width));
     }
 }
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
