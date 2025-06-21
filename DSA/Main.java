35e2612ed40e54506215da8454fca97b47b8de73
sesdevAchary <sesdev.achary@gmail.com>
2025-06-21 17:01:26 +0000

    added the new ArrayList

--- DSA/Main.java
+++ DSA/Main.java
@@ -539,6 +539,20 @@
 
 
 
+import java.util.*;
+
+class Main{
+        public static void main (String[] args) {
+            ArrayList<Integer> a = new ArrayList<Integer>
+        }
+
+}
+
+
+
+
+
+
 
 
 
