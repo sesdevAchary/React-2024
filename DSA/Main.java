3be8d0596f3a36da02c0630c1485b58b2547b903
sesdevAchary <sesdev.achary@gmail.com>
2025-06-21 17:04:10 +0000

    the new array List formedv

--- DSA/Main.java
+++ DSA/Main.java
@@ -543,7 +543,15 @@ import java.util.*;
 
 class Main{
         public static void main (String[] args) {
-            ArrayList<Integer> a = new ArrayList<Integer>
+            ArrayList<Integer> a = new ArrayList<Integer>();
+
+
+            a.add(1);
+            a.add(2);
+            a.add(3);
+
+                  	System.out.println(a);
+
         }
 
 }
