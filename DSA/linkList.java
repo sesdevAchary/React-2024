a510bc3a0a6191b741a493851b93ae90cf2aa0e1
sesdevAchary <sesdev.achary@gmail.com>
2025-06-27 12:08:25 +0000

    mergin the 2 sortedd array then returning a new array

--- DSA/linkList.java
+++ DSA/linkList.java
@@ -358,7 +358,27 @@ class linkList{
 
     }
 
+public static void mergeTwoList(Node head1, Node head2){
+    // takes 2 sorted LL as params then return head of a new merged sorted list//
+    Node dummy = new Node(-1);
+    Node tail = dummy ;
 
+
+    while( head1!= null && head2!= null){
+        if(head1.data <= head2.data){    // if h1<h2 => dummy- list1-list2
+            tail.next= head1;
+            head1= head1.next;
+        }
+        else{                           // if h2<h1 => dummy- list2-list1
+            tail.next = head2;
+            head2 = head2.next;
+        }
+        tail = tail.next;
+    }
+ 
+    if(head1 !=null)tail.next=h1;
+    if(head2 !=null)tail.next=h2;
+}
 
     public void printList(){
         Node current = head;
