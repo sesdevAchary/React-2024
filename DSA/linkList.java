74945f552c17bd0e074313de55993ad6c2ca8e2d
sesdevAchary <sesdev.achary@gmail.com>
2025-06-29 16:12:28 +0000

    inserting a new node at the start

--- DSA/linkList.java
+++ DSA/linkList.java
@@ -546,3 +546,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+def insert_at_beginning(head, data):
+    new_node = Node(data)
+    new_node.next = head
+    return new_node
