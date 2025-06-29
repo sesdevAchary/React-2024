08bc99e7fd6bb6e1cbcc32adf05afc481a2e4291
sesdevAchary <sesdev.achary@gmail.com>
2025-06-29 16:12:58 +0000

    inserting a new node at the ending part

--- DSA/linkList.java
+++ DSA/linkList.java
@@ -550,3 +550,12 @@ def insert_at_beginning(head, data):
     new_node = Node(data)
     new_node.next = head
     return new_node
+def insert_at_end(head, data):
+    new_node = Node(data)
+    if not head:
+        return new_node
+    temp = head
+    while temp.next:
+        temp = temp.next
+    temp.next = new_node
+    return head
