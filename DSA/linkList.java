6795f7ac8956806ab300d5114777d408884cd025
sesdevAchary <sesdev.achary@gmail.com>
2025-06-29 16:17:51 +0000

    merging the 2 sorted list

--- DSA/linkList.java
+++ DSA/linkList.java
@@ -587,3 +587,14 @@ def length(head):
         count += 1
         head = head.next
     return count
+def merge_lists(l1, l2):
+    dummy = Node(0)
+    tail = dummy
+    while l1 and l2:
+        if l1.data < l2.data:
+            tail.next, l1 = l1, l1.next
+        else:
+            tail.next, l2 = l2, l2.next
+        tail = tail.next
+    tail.next = l1 or l2
+    return dummy.next
