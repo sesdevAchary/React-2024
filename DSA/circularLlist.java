9387d2643708ce8de66bd9940e10c047ad975599
sesdevAchary <sesdev.achary@gmail.com>
2025-07-03 06:40:22 +0000

    nul head checker

--- DSA/circularLlist.java
+++ DSA/circularLlist.java
@@ -19,7 +19,22 @@ public class circularLlist {
 
         if(head == null ){
             head = newNd;tail=newNd;
-            newNd.next= head;
+            newNd.next= head;  //ircular reference.//
         }
+        else{
+            tail.next = newNd;    //Link the current last node to the new one//
+            tail= newNd;          //Update the tail reference points to the new node //
+            tail.next=head;       //Re-link the new tail back to the head//
+            
+
+
+            // [10] → [20] → [30] → [40] ─┐
+            //   ^                        |
+            //   |________________________|
+            //  head              tail
+
+
+
+        }     
     }
 }
