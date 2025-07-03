c7efc4f197a08471e8d44f6f7519e85e02c984bc
sesdevAchary <sesdev.achary@gmail.com>
2025-07-03 04:15:24 +0000

    linkedlist for Node value

---
+++ DSA/circularLlist.java
@@ -0,0 +1,18 @@
+public class circularLlist {
+    Node head = null ;   //Points to the first node.
+    Node tail = null;   //Points to the last node.
+
+
+    class Node {
+        int data ;
+        Node next;
+
+        Node ( int data){
+            this.data= data;
+            this.next= null;
+        }
+    }
+
+
+    
+}
