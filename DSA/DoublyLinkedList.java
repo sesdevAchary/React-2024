794e9b627a27e6d26b4278b6e6eabb009cd09108
sesdevAchary <sesdev.achary@gmail.com>
2025-07-06 05:27:06 +0000

    doubly L list with node class

--- DSA/DoublyLinkedList.java
+++ DSA/DoublyLinkedList.java
@@ -1,14 +1,18 @@
-class DoublyLlist{
+class DoublyLinkedList{
     Node head;
 
     class Node{
         int data; Node next; Node prev;
 
         Node(int data){
-            this.data=null;
+            this.data=data;
             this.next=null;
             this.prev=null;
         }
 
+    }
+
+     public DoublyLinkedList() {
+        head = null;
     }
 }
