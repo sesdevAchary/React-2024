fe6b1f39fc14ee17293e4972b73c24642494df58
sesdevAchary <sesdev.achary@gmail.com>
2025-07-06 05:32:50 +0000

    appending code format

--- DSA/DoublyLinkedList.java
+++ DSA/DoublyLinkedList.java
@@ -15,4 +15,28 @@ class DoublyLinkedList{
      public DoublyLinkedList() {
         head = null;
     }
+
+
+
+    public void append( int data){
+        Node newNd = new Node(data);
+
+        if( head == null){
+            head = newNd;
+            return;
+        }
+
+        Node current = head;
+        while(current != null){
+            current = current.next;
+        }
+
+        current.next= newNd;
+        newNd.prev = current;
+    }
+
+
+
+
+    
 }
