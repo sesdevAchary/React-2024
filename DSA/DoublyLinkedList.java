ceba891f060e18d68345124cb4653a9f5d2feab9
sesdevAchary <sesdev.achary@gmail.com>
2025-07-06 17:27:42 +0000

    printing the entire list

--- DSA/DoublyLinkedList.java
+++ DSA/DoublyLinkedList.java
@@ -1,52 +1,52 @@
-class DoublyLinkedList{
+public class DoublyLinkedList {
     Node head;
 
-    class Node{
-        int data; Node next; Node prev;
+    class Node {
+        int data;
+        Node next;
+        Node prev;
 
-        Node(int data){
-            this.data=data;
-            this.next=null;
-            this.prev=null;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
         }
-
     }
 
-     public DoublyLinkedList() {
+    public DoublyLinkedList() {
         head = null;
     }
 
-
-
-    public void append( int data){
+    public void add(int data) {
         Node newNd = new Node(data);
 
-        if( head == null){
+        if (head == null) {
             head = newNd;
             return;
         }
 
         Node current = head;
-        while(current != null){
+
+        // Traverse till the last node
+        while (current.next != null) {
             current = current.next;
         }
 
-        current.next= newNd;
+        current.next = newNd;
         newNd.prev = current;
     }
 
-public void printForward(){
-    Node current = head ;
-    System.out.println(" printing the dll");
-    while( current != null){
-        System.out.print(current.data + " ");
-    current=current.next;
+    public void printForward() {
+        Node current = head;
+        System.out.println("Printing the DLL:");
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
     }
-    System.out.println();
-}
 
-
-    public static void main ( String args[]){
+    public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList();
 
         dll.add(5);
@@ -56,8 +56,6 @@ public void printForward(){
         dll.add(40);
         dll.add(50);
         dll.add(60);
-
-
 
         dll.printForward();
     }
