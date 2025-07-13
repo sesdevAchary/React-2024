7f7d3a62781565e18df66741300fa3865b844e9a
sesdevAchary <sesdev.achary@gmail.com>
2025-07-13 16:56:08 +0000

    sorting the list

--- DSA/DoublyLinkedList.java
+++ DSA/DoublyLinkedList.java
@@ -591,5 +591,32 @@ public Node flatten(Node head) {
     }
     return head;
 }
+public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode slow = head, fast = head, prev = null;
+    while (fast != null && fast.next != null) {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    prev.next = null;
+    ListNode l1 = sortList(head);
+    ListNode l2 = sortList(slow);
+    return merge(l1, l2);
+}
+
+ListNode merge(ListNode l1, ListNode l2) {
+    ListNode dummy = new ListNode(0), curr = dummy;
+    while (l1 != null && l2 != null) {
+        if (l1.val < l2.val) {
+            curr.next = l1; l1 = l1.next;
+        } else {
+            curr.next = l2; l2 = l2.next;
+        }
+        curr = curr.next;
+    }
+    curr.next = (l1 != null) ? l1 : l2;
+    return dummy.next;
+}
 
 
