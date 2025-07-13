51d4f0687fe2a2aa3b1c5e2b599b9ea5e5524211
sesdevAchary <sesdev.achary@gmail.com>
2025-07-13 17:08:06 +0000

    n queen problem

--- DSA/DoublyLinkedList.java
+++ DSA/DoublyLinkedList.java
@@ -693,4 +693,37 @@ class Solution {
         b[i][j] = c;
     }
 }
+public List<List<String>> solveNQueens(int n) {
+    List<List<String>> res = new ArrayList<>();
+    char[][] board = new char[n][n];
+    for (char[] row : board) Arrays.fill(row, '.');
+    backtrack(0, board, res);
+    return res;
+}
+
+void backtrack(int row, char[][] board, List<List<String>> res) {
+    if (row == board.length) {
+        List<String> config = new ArrayList<>();
+        for (char[] r : board) config.add(new String(r));
+        res.add(config);
+        return;
+    }
+    for (int col = 0; col < board.length; col++) {
+        if (isSafe(board, row, col)) {
+            board[row][col] = 'Q';
+            backtrack(row + 1, board, res);
+            board[row][col] = '.';
+        }
+    }
+}
+
+boolean isSafe(char[][] board, int row, int col) {
+    for (int i = 0; i < row; i++)
+        if (board[i][col] == 'Q') return false;
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j] == 'Q') return false;
+    for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++)
+        if (board[i][j] == 'Q') return false;
+    return true;
+}
 
