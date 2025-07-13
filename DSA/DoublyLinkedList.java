9ad5f8a73695b58fe74657403d8aeebafc5aef8c
sesdevAchary <sesdev.achary@gmail.com>
2025-07-13 17:04:41 +0000

    palindrome the linked list

--- DSA/DoublyLinkedList.java
+++ DSA/DoublyLinkedList.java
@@ -630,5 +630,25 @@ public ListNode removeNthFromEnd(ListNode head, int n) {
     slow.next = slow.next.next;
     return dummy.next;
 }
+public boolean isPalindrome(ListNode head) {
+    ListNode slow = head, fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    ListNode prev = null;
+    while (slow != null) {
+        ListNode next = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = next;
+    }
+    while (prev != null) {
+        if (head.val != prev.val) return false;
+        head = head.next;
+        prev = prev.next;
+    }
+    return true;
+}
 
 
