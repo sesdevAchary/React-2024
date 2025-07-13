5e67166aa8e1ed6aef9c92ad1b278b7af96e9885
sesdevAchary <sesdev.achary@gmail.com>
2025-07-13 14:39:53 +0000

    reversin the k map

--- DSA/DoublyLinkedList.java
+++ DSA/DoublyLinkedList.java
@@ -471,6 +471,27 @@ public ListNode reverseKGroup(ListNode head, int k) {
     }
     return prev;
 }
+class Node {
+    int val;
+    Node next, random;
+    Node(int val) { this.val = val; }
+}
+
+public Node copyRandomList(Node head) {
+    Map<Node, Node> map = new HashMap<>();
+    Node cur = head;
+    while (cur != null) {
+        map.put(cur, new Node(cur.val));
+        cur = cur.next;
+    }
+    cur = head;
+    while (cur != null) {
+        map.get(cur).next = map.get(cur.next);
+        map.get(cur).random = map.get(cur.random);
+        cur = cur.next;
+    }
+    return map.get(head);
+}
 
 
 
