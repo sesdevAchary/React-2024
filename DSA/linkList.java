c5d4d983158866bcc2e1f67a2008f90adaf233fb
sesdevAchary <sesdev.achary@gmail.com>
2025-09-03 13:40:16 +0000

    hadling the exception

--- DSA/linkList.java
+++ DSA/linkList.java
@@ -973,53 +973,88 @@ public Node getIntersection(){
     return a;
 }
 
-public void reorderList(ListNode head) {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
 
-        public ListNode middle(ListNode head){
-            if(head==null || head->next == null)
+        if (head == null || head.next == null) {
             return;
-            ListNode slow=head,fast=head;
-        while(fast!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
         }
 
+        ListNode middle = middle(head);
+
+        ListNode secondHead = middle.next;
+        middle.next = null;
+        secondHead = reverse(secondHead);
 
         
+        merge(head,secondHead);
+
     }
-     private ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode nextTemp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextTemp;
-        }
-        return prev;
-    }
-private void mergeInPlace(ListNode l1, ListNode l2) {
-        while (l2 != null) {
-            ListNode temp1 = l1.next;
-            ListNode temp2 = l2.next;
-            
-            l1.next = l2;
-            l2.next = temp1;
-            
-            l1 = temp1;
-            l2 = temp2;
-        }
-    }  private ListNode findMiddle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+
+    public ListNode middle(ListNode head) {
+
+        
+        ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
     }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    public ListNode merge(ListNode head1,ListNode head2){
+        if(head1==null)return head2;
+        if(head2==null)return head1;
+
+        ListNode p1=head1; ListNode p2=head2;
+        ListNode dummyHead= new ListNode(); ListNode pMerge=dummyHead;
+
+        while(p1 != null && p2 != null){
+           if(p1.val<p2.val){
+            pMerge.next=p1;
+            p1=p1.next;
+           }
+           else{
+            pMerge.next=p2;p2=p2.next;
+           }
+           pMerge=pMerge.next;
+        }
+        if (p1 != null) {
+        pMerge.next = p1;
+    } else if (p2 != null) {
+        pMerge.next = p2;
+    }
+
+    return dummyHead.next;
+
+    } 
+      
+}
 
 
 }
