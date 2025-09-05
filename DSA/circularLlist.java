c74d8e88f971bb4b90c9e6e23618ae1f7d6f9dda
sesdevAchary <sesdev.achary@gmail.com>
2025-09-05 11:47:51 +0000

    searching for boolean value

--- DSA/circularLlist.java
+++ DSA/circularLlist.java
@@ -11,86 +11,100 @@ public class circularLlist{
         }
     }
 
-    public void addLast(int data ){
-        Node newhd= new Node(data);
-        if(head == null){
-            head=newhd;tail=newhd;newhd.next=head;
-        }
-        else{
-            tail.next=newhd;  // Link the curr last node to the new one//
-            tail=newhd;      //Update the tail reference points to the new node //
-            tail.next=head;  //Re-link the new tail back to the head//
-        }
+//     public void addLast(int data ){
+//         Node newhd= new Node(data);
+//         if(head == null){
+//             head=newhd;tail=newhd;newhd.next=head;
+//         }
+//         else{
+//             tail.next=newhd;  // Link the curr last node to the new one//
+//             tail=newhd;      //Update the tail reference points to the new node //
+//             tail.next=head;  //Re-link the new tail back to the head//
+//         }
+//     }
+// //             // [10] → [20] → [30] → [40] ─┐
+// //             //   ^                        |
+// //             //   |________________________|
+// //             //  head              tail
+
+// public void deleteLast(){
+//     if( head == null ){
+//         System.out.println( " empty list ");
+//     }
+
+//     if ( head == tail ){
+//         head = null ;
+//         tail = null;
+//     }else {
+//         Node current = head ;
+//         while ( current.next!= tail){
+//             current= current.next;
+
+//         }
+//         current.next = head ;
+//         tail = current;
+//     }
+
+
+// }
+//     public void deleteFromFirst(){
+//         if ( head == null){
+//             System.out.print(" empty list");
+//             return;
+//         }
+//         if( head == tail){
+//             head=null;
+//             tail=null;
+//         }else{
+//             head=head.next;
+//             tail.next=head;
+//         }
+
+//     }
+
+//     public void addFirst(int data){
+//         Node newNd= new Node (data);
+
+//         if( head == null){
+//             head = newNd; tail= newNd;newNd.next= head;
+//         }
+//         else{
+//             newNd.next=head; // Point new node to current head	Connects new to old first node//
+//             head=newNd;     //  Update head to new node	Makes new node the first in the list//
+//             tail.next=head;  // Update circular link	Makes last node point to new head//
+//         }
+//     }
+
+// public void insertAtSpecificPlace(int data, int position){
+//     if(position <=1 || head== null){
+//         addFirst(data);
+//         return;
+//     }
+//     int index=0;Node current=head;
+//     while( index<position-1 && current.next != null){
+//         index+=1;current=current.next;
+//     }
+
+//     if(current.next == head)
+//     addLast(data);  //If the loop hit the end of the list before reaching position − 1, just add to the end.
+//     else{
+//         Node newhd= new Node(data);
+//         newhd.next=current.next;
+//         current.next=newhd;
+//     }
+// }
+
+public Node searchVal(int target){
+    if ( head == null){
+        System.out.println("empty");
     }
-//             // [10] → [20] → [30] → [40] ─┐
-//             //   ^                        |
-//             //   |________________________|
-//             //  head              tail
-
-public void deleteLast(){
-    if( head == null ){
-        System.out.println( " empty list ");
-    }
-
-    if ( head == tail ){
-        head = null ;
-        tail = null;
-    }else {
-        Node current = head ;
-        while ( current.next!= tail){
-            current= current.next;
-
-        }
-        current.next = head ;
-        tail = current;
-    }
-
-
-}
-    public void deleteFromFirst(){
-        if ( head == null){
-            System.out.print(" empty list");
-            return;
-        }
-        if( head == tail){
-            head=null;
-            tail=null;
-        }else{
-            head=head.next;
-            tail.next=head;
-        }
-
-    }
-
-    public void addFirst(int data){
-        Node newNd= new Node (data);
-
-        if( head == null){
-            head = newNd; tail= newNd;newNd.next= head;
-        }
-        else{
-            newNd.next=head; // Point new node to current head	Connects new to old first node//
-            head=newNd;     //  Update head to new node	Makes new node the first in the list//
-            tail.next=head;  // Update circular link	Makes last node point to new head//
-        }
-    }
-
-public void insertAtSpecificPlace(int data, int position){
-    if(position <=1 || head== null){
-        addFirst(data);
-        return;
-    }
-    int index=0;Node current=head;
-    while( index<position-1 && current.next != null){
-        index+=1;current=current.next;
-    }
-
-    if(current.next == head)
-    addLast(data);  //If the loop hit the end of the list before reaching position − 1, just add to the end.
     else{
-        Node newhd= new Node(data);
-        newhd.next=current.next;
-        current.next=newhd;
+        Node curr=head;
+        do{
+            if(curr == target)
+            return curr;
+
+        }while(head != curr)
     }
 }
 
@@ -109,20 +123,23 @@ System.out.println("(back to head)");
 public static void main(String[] args){
     circularLlist cll=new circularLlist();
 
-    cll.addLast(12);
-    cll.addLast(13);
-    cll.addLast(14);
-    System.out.println("After adding last:"); //  12 -> 13 -> 14 -> (back to head)
-    cll.printList();
+    // cll.addLast(12);
+    // cll.addLast(13);
+    // cll.addLast(14);
+    // System.out.println("After adding last:"); //  12 -> 13 -> 14 -> (back to head)
+    // cll.printList();
 
-    cll.addFirst(17);
-    cll.addFirst(5);
-    System.out.println("After adding first:"); //5 -> 17 -> 12 -> 13 -> 14 -> (back to head)
-    cll.printList();
+    // cll.addFirst(17);
+    // cll.addFirst(5);
+    // System.out.println("After adding first:"); //5 -> 17 -> 12 -> 13 -> 14 -> (back to head)
+    // cll.printList();
 
-    cll.insertAtSpecificPlace(15, 3);    //  5 -> 17 -> 12 -> 15 -> 13 -> 14 -> (back to head)
-    System.out.println("inserting at index 3:");
-    cll.printList();    
+    // cll.insertAtSpecificPlace(15, 3);    //  5 -> 17 -> 12 -> 15 -> 13 -> 14 -> (back to head)
+    // System.out.println("inserting at index 3:");
+    // cll.printList();    
+
+    cll.searchVal(12);
+    cll.printList();  
 
 
 }
@@ -194,48 +211,6 @@ public static void main(String[] args){
 //         System.out.println("back to head");
 //     }
 
-
-
-//     public static void main ( String args[]){
-//         circularLlist cll = new circularLlist();
-
-//         cll.addLast(10);
-//         cll.addLast(20);
-//         cll.addLast(30);
-//         System.out.println("After adding last:");
-
-
-        
-
-
-//         cll.deleteFromFirst();
-//         System.out.println("After deleting first:");
-
-//         cll.deleteLast();
-//         System.out.println("After deleting last:");
-
-
-
-
-
-//     //    cll.update(10,90);
-//     //    System.out.println("updating old value, the new value is:");
-
-//     int updateIndex = cll.update(10,70);
-
-//     if ( updateIndex != -1){
-//         System.out.println(" value updated at index" + updateIndex);
-//     }
-//     else{
-//         System.out.println(" value not found");
-//     }
-
-
-//         cll.printList();
-
-
-//     }
-// }
 
 
 
