f718984c6b75dc000ca57a0a06ff1cdc919f25b3
sesdevAchary <sesdev.achary@gmail.com>
2025-09-19 12:52:22 +0000

    pushing the main element

--- DSA/stack/sampleStack.java
+++ DSA/stack/sampleStack.java
@@ -1,7 +1,27 @@
 public class sampleStack{
     static class Node{
         int data;Node next;
-        
+        public Node(int data){
+            this.data=data;
+            Next=null;
+        }
+
+    }
+
+    static class Stack{
+        public Node head;
+        public static boolean isEmpty(){
+            return head == null;
+        }
+        public void push(int data){
+            Node newd=new Node(data);
+            if(isEmpty()){
+                head=newd;
+                return;
+            }
+            newd.next=head;head=newd;
+
+        }
     }
 
 
