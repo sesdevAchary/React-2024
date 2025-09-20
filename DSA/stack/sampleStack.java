c0418506577aacca78b27dcb214d1b42a035a9a0
sesdevAchary <sesdev.achary@gmail.com>
2025-09-20 16:56:29 +0000

    figurig out the node

--- DSA/stack/sampleStack.java
+++ DSA/stack/sampleStack.java
@@ -1,14 +1,14 @@
 public class sampleStack{
     // the box is used
-    static class Node{
-        int data;Node next;
-        public Node(int data){
-            this.data=data;
-            next=null;
-            // the plates 
-        }
+    // static class Node{
+    //     int data;Node next;
+    //     public Node(int data){
+    //         this.data=data;
+    //         next=null;
+    //         // the plates 
+    //     }
 
-    }
+    // }
 
     static class Stack{
         // public Node head;
