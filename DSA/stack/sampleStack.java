2a0173faae7cb40b5c24fefdf0ae97180374ab7d
sesdevAchary <sesdev.achary@gmail.com>
2025-09-20 16:54:45 +0000

    pushing the main element with altered version

--- DSA/stack/sampleStack.java
+++ DSA/stack/sampleStack.java
@@ -1,53 +1,59 @@
 public class sampleStack{
+    // the box is used
     static class Node{
         int data;Node next;
         public Node(int data){
             this.data=data;
             next=null;
+            // the plates 
         }
 
     }
 
     static class Stack{
-        public Node head;
+        // public Node head;
 
 
-        public boolean isEmpty(){
-            return head == null;
-        }
-        public void push(int data){
-            Node newd=new Node(data);
-            if(isEmpty()){
-                head=newd;
-                return;
-            }
-            newd.next=head;head=newd;
+        // public boolean isEmpty(){
+        //     return head == null;
+        // }
+        // public void push(int data){
+        //     Node newd=new Node(data);
+        //     if(isEmpty()){
+        //         head=newd;
+        //         return;
+        //     }
+        //     newd.next=head;head=newd;
 
-        }
+        // }
 
-        public static int pop(){
-            if(isEmpty())
-            return -1;
+        // public static int pop(){
+        //     if(isEmpty())
+        //     return -1;
 
-            int top=head.data;
-            head=head.next;
-            return top;
-        }
+        //     int top=head.data;
+        //     head=head.next;
+        //     return top;
+        // }
 
-         public static int peek(){
-            if(isEmpty())
-            return -1;
+        //  public static int peek(){
+        //     if(isEmpty())
+        //     return -1;
 
-            return head.data;
-        }
+        //     return head.data;
+        // }
 
 
     }
 
     public static void main(String args[]){
-        Stack s=new Stack();
+        // Stack s=new Stack();
 
-        s.push(1);s.push(2);s.push(3);s.push(4);s.push(5);s.push(6);s.push(7);s.push(8);
+        // s.push(1);s.push(2);s.push(3);s.push(4);s.push(5);s.push(6);s.push(7);s.push(8);
+
+        Stack s = new Stack();
+        s.push(10);
+        System.out.println(s.pop());
 
 
     }
@@ -55,3 +61,6 @@ public class sampleStack{
 
 
 }
+
+
+
