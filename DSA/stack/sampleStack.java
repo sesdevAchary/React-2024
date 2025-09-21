cd5c3b63e87a4a8a69df898b0a79050ab916ce43
sesdevAchary <sesdev.achary@gmail.com>
2025-09-21 16:09:18 +0000

    abbreveation post

--- DSA/stack/sampleStack.java
+++ DSA/stack/sampleStack.java
@@ -1,18 +1,46 @@
 import java.util.*;
 
 public class sampleStack{
-   public static void main(String args[]){
-    Stack <Integer> s= new Stack<>();
+//    public static void main(String args[]){
+//     Stack <Integer> s= new Stack<>();
 
-    s.push(1);s.push(2);s.push(3);s.push(4);
+//     s.push(1);s.push(2);s.push(3);s.push(4);
 
-    while(!s.isEmpty()){
-        System.out.println(peek());
-        s.pop();
+//     while(!s.isEmpty()){
+//         System.out.println(s.peek());
+//         s.pop();
+//     }
+//    }
+
+
+public static void pushAtBottom( int data, Stack<Integer> s){
+    if(s.isEmpty())
+    {
+        s.push(data)
+        return ;
     }
-   }
 
+    int top=s.pop();
+    pushAtBottom(data,s);
+    s.push(top);
 }
+
+public static void reverse(Stack<Integer> s){
+    if(s.isEmpty()){
+        return;
+    }
+
+    int top=s.pop();
+    reverse(s);
+    pushAtBottom(top,s);
+}
+
+public static void main(String args[]){
+    stack <Integer> s= new stack<>();
+    s.push(1);s.push(2);s.push(3);s.push(4)
+}
+}
+
 
 
 
