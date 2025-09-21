0737f73f55c598328959265b1b9a880196dbb82f
sesdevAchary <sesdev.achary@gmail.com>
2025-09-21 13:19:50 +0000

    abbreveation post

--- DSA/stack/sampleStack.java
+++ DSA/stack/sampleStack.java
@@ -1,69 +1,16 @@
+import java.util.*;
+
 public class sampleStack{
-    // the box is used
-    static class Node{
-        int data;Node next;
-        public Node(int data){
-            this.data=data;
-            next=null;
-            // the plates 
-        }
+   public static void main(String args[]){
+    Stack <Integer> s= new Stack<>();
 
+    s.push(1);s.push(2);s.push(3);s.push(4);
+
+    while(!s.isEmpty()){
+        System.out.println(peek());
+        s.pop();
     }
-
-    static class Stack{
-        // public Node head;
-
-
-        // public boolean isEmpty(){
-        //     return head == null;
-        // }
-        // public void push(int data){
-        //     Node newd=new Node(data);
-        //     if(isEmpty()){
-        //         head=newd;
-        //         return;
-        //     }
-        //     newd.next=head;head=newd;
-
-        // }
-
-        public int pop() {
-            if (isEmpty()) return -1;
-            int top = head.data;
-            head = head.next;
-            return top;
-        }
-        
-
-        public int peek() {
-            if (isEmpty()) return -1;
-            return head.data;
-        }
-    
-
-        //  public static int peek(){
-        //     if(isEmpty())
-        //     return -1;
-
-        //     return head.data;
-        // }
-
-
-    }
-
-    public static void main(String args[]){
-        // Stack s=new Stack();
-
-        // s.push(1);s.push(2);s.push(3);s.push(4);s.push(5);s.push(6);s.push(7);s.push(8);
-
-        Stack s = new Stack();
-        s.push(10);
-        System.out.println(s.push());
-
-
-    }
-
-
+   }
 
 }
 
