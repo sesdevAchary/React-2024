2c485d5aff5a7d1ed471e44daf70dc44a0d8a647
sesdevAchary <sesdev.achary@gmail.com>
2025-10-03 17:19:30 +0000

    checking for isEmpty method

--- DSA/queueY/queueClass.java
+++ DSA/queueY/queueClass.java
@@ -1,6 +1,18 @@
 public class queueClass{
     static class queue{
-        
+        static int arr[];
+        static int size;
+        static int rear=-1;
+
+        queue(int n){
+            arr= new  int[n];
+            this.size=n;
+            
+        }
+
+        public static boolean isEmpty(){
+             return rear == -1;
+        }
 
     }
 
