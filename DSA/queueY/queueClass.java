2e594c2a82b32d3e8f2caf5e159765d39c947868
sesdevAchary <sesdev.achary@gmail.com>
2025-10-04 11:49:35 +0000

    seeking for the main function

--- DSA/queueY/queueClass.java
+++ DSA/queueY/queueClass.java
@@ -51,8 +51,10 @@ public class queueClass{
         queue q= new queue(5);
         q.add(1);q.add(2);q.add(3);
 
-        System.out.println(q.peek());
+        while ( !q.isEmpty()){
+            System.out.println(q.peek());
         q.delete();
+        }
 
 
     }
