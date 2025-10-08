3c0d1ee19764c4bc82276168b91150c68f8bdd84
sesdevAchary <sesdev.achary@gmail.com>
2025-10-08 13:49:18 +0000

    2 stack operation

--- DSA/queueY/practiceQueue.java
+++ DSA/queueY/practiceQueue.java
@@ -2,11 +2,27 @@ import java.util.*;
 public class practiceQueue {
 // QUEUE using 2 STACKS//
 /*q-1 poppig out element from s1 then add new element in s1 again push all  */
-   public class queueY{
-    Stack<Integer> s1= new Stack<>();
-    Stack<Integer> s2= new Stack<>();
+   static class queueY{
+    static Stack<Integer> s1= new Stack<>();
+    static Stack<Integer> s2= new Stack<>();
 
-    
+    public static boolean isEmpty(){
+        return s1.isEmpty();
+    }
+
+    public static void add(int data){
+        if(!s1.isEmpty())
+        {
+            s2.push(s1.pop());
+        }
+
+        s1.push(data);
+
+        if(!s2.isEmpty()){
+            s1.push(s2.pop());
+            
+        }
+    }
    }
     
 }
