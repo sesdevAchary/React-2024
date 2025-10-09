4b1235273620aba52b8f1a613f9f43fc71f17f51
sesdevAchary <sesdev.achary@gmail.com>
2025-10-09 14:35:06 +0000

    obtaining the circular  queue

--- DSA/queueY/queueClass.java
+++ DSA/queueY/queueClass.java
@@ -1,205 +1,219 @@
 import java.util.*;
-public class queueClass{
-//     static class queue{
-//         static int arr[];
-//         static int size;
-//         static int rear=-1; static int front = -1;
+ public class queueClass{
+// //     static class queue{
+// //         static int arr[];
+// //         static int size;
+// //         static int rear=-1; static int front = -1;
 
-//         queue(int n){
-//             arr= new  int[n];
-//             this.size=n;
-//        }
-// ////////////////// FOR NORMAL QUEUE //////////////////////
-//         // public static boolean isEmpty(){
-//         //      return rear == -1;
-//         // }
+// //         queue(int n){
+// //             arr= new  int[n];
+// //             this.size=n;
+// //        }
+// // ////////////////// FOR NORMAL QUEUE //////////////////////
+// //         // public static boolean isEmpty(){
+// //         //      return rear == -1;
+// //         // }
 
-//         // public static void add(int data){
-//         //     if(rear == size -1){
-//         //         System.out.println (" Queue is already full my Lord");
-//         //     }
+// //         // public static void add(int data){
+// //         //     if(rear == size -1){
+// //         //         System.out.println (" Queue is already full my Lord");
+// //         //     }
 
-//         //     rear++;
-//         //     arr[rear]=data;
-//         // }
+// //         //     rear++;
+// //         //     arr[rear]=data;
+// //         // }
 
-//         // public static int delete(){
-//         //     if(isEmpty()){
-//         //         System.out.println("Queue is already the least");
-//         //         return -1;
-//         //     }
+// //         // public static int delete(){
+// //         //     if(isEmpty()){
+// //         //         System.out.println("Queue is already the least");
+// //         //         return -1;
+// //         //     }
 
-//         //     int front=arr[0];
-//         //     for(int i=0;i<rear;i++){
-//         //         arr[i]=arr[i+1];
+// //         //     int front=arr[0];
+// //         //     for(int i=0;i<rear;i++){
+// //         //         arr[i]=arr[i+1];
                 
-//         //     }
-//         //     rear --;
-//         //     return front;
-//         // }
-//         // public static int peek(){
-//         //     if(isEmpty()){
-//         //         System.out.println("Queue is already the least");
-//         //         return -1;
-//         //     }
-//         //     return arr[0];
-//         // }
+// //         //     }
+// //         //     rear --;
+// //         //     return front;
+// //         // }
+// //         // public static int peek(){
+// //         //     if(isEmpty()){
+// //         //         System.out.println("Queue is already the least");
+// //         //         return -1;
+// //         //     }
+// //         //     return arr[0];
+// //         // }
 
 
-//         ////////////////// FOR CIRCULAR  QUEUE //////////////////////
-//         // public static boolean isEmpty(){
-//         //     return  front == -1 && rear == -1 ;
-//         // }
-//         // public static boolean isFull(){
-//         //     return (rear +1 ) % size == front;
-//         // }
+// //         ////////////////// FOR CIRCULAR  QUEUE //////////////////////
+// //         // public static boolean isEmpty(){
+// //         //     return  front == -1 && rear == -1 ;
+// //         // }
+// //         // public static boolean isFull(){
+// //         //     return (rear +1 ) % size == front;
+// //         // }
 
-//         // public static void add(int data){
-//         //     if(isFull()){
-//         //         System.out.println("Already Full)");return;
+// //         // public static void add(int data){
+// //         //     if(isFull()){
+// //         //         System.out.println("Already Full)");return;
 
-//         //     }
+// //         //     }
 
-//         //     // for 1st element //
-//         //     if(front == -1)
-//         //     {
-//         //         front=0;
-//         //     }
+// //         //     // for 1st element //
+// //         //     if(front == -1)
+// //         //     {
+// //         //         front=0;
+// //         //     }
 
-//         //     rear=(rear+1)%size;
-//         //     arr[rear]=data;
-//         // }
-//         // public static int  delete(){
-//         //     if(isEmpty()){
-//         //         System.out.println("Already empty sorry ");return -1;
-//         //     }
-//         //     int result=arr[front];
+// //         //     rear=(rear+1)%size;
+// //         //     arr[rear]=data;
+// //         // }
+// //         // public static int  delete(){
+// //         //     if(isEmpty()){
+// //         //         System.out.println("Already empty sorry ");return -1;
+// //         //     }
+// //         //     int result=arr[front];
 
-//         //     if(front ==rear){
-//         //         rear=front=-1;
-//         //     }else{
-//         //         front=(front+1)%size;
-//         //     }
-//         //     return result;
-//         // }
+// //         //     if(front ==rear){
+// //         //         rear=front=-1;
+// //         //     }else{
+// //         //         front=(front+1)%size;
+// //         //     }
+// //         //     return result;
+// //         // }
 
-//         // public static int peek(){
-//         //     if(isEmpty()){
-//         //         System.out.println("Already empty sorry ");return -1;
-//         //     }
-//         //     return arr[front];
-//         // }
-
-
-//     }
-
-//     public static void main ( String[] args){
-//         queue q= new queue(5);
-//         q.add(1);q.add(2);q.add(3);q.add(4);q.add(5);
-//         System.out.println(q.delete());
-//         q.add(6);
-//         System.out.println(q.delete());
-//         q.add(7);
-//         //12345 then 62345 then 76345
+// //         // public static int peek(){
+// //         //     if(isEmpty()){
+// //         //         System.out.println("Already empty sorry ");return -1;
+// //         //     }
+// //         //     return arr[front];
+// //         // }
 
 
-//         while ( !q.isEmpty()){
-//             System.out.println(q.peek());
-//         q.delete();
-//         }
+// //     }
+
+// //     public static void main ( String[] args){
+// //         queue q= new queue(5);
+// //         q.add(1);q.add(2);q.add(3);q.add(4);q.add(5);
+// //         System.out.println(q.delete());
+// //         q.add(6);
+// //         System.out.println(q.delete());
+// //         q.add(7);
+// //         //12345 then 62345 then 76345
 
 
-//     }
-//         ////////////////// FOR   QUEUE  USING LINKEDLIST //////////////////////
+// //         while ( !q.isEmpty()){
+// //             System.out.println(q.peek());
+// //         q.delete();
+// //         }
 
-//     static class Node{  // CREATING node for linklist//
-//     int data;
-//     Node next;
 
-//     Node(int data){
-//         this.data=data;
-//         next=null;
-//     }
-//    }
+// //     }
+// //         ////////////////// FOR   QUEUE  USING LINKEDLIST //////////////////////
 
-//    static class queue{  // creating queue class of variable size cause linkedlist is of variable size//
-//     static Node head=null;
-//     static Node tail=null;
+// //     static class Node{  // CREATING node for linklist//
+// //     int data;
+// //     Node next;
 
-//     static boolean isEmpty(){
-//         return (head == null & tail == null);
-//     }
+// //     Node(int data){
+// //         this.data=data;
+// //         next=null;
+// //     }
+// //    }
 
-//     public static void  add(int data)
+// //    static class queue{  // creating queue class of variable size cause linkedlist is of variable size//
+// //     static Node head=null;
+// //     static Node tail=null;
+
+// //     static boolean isEmpty(){
+// //         return (head == null & tail == null);
+// //     }
+
+// //     public static void  add(int data)
+// //     {
+// //         Node nd= new Node(data);
+
+// //         if(tail == null)
+// //         {
+// //             head=tail=nd;
+// //         }
+// //         tail.next=nd;
+// //         tail=nd;
+// //     }
+// //     public static int delete(){
+// //         if(isEmpty()){
+// //             System.out.println("Already empty you cant delete anything ");
+// //             return -1;
+// //         }
+// //         int front=head.data;
+
+// //         if(head== null)
+// //         tail=null;
+
+// //         head=head.next;
+// //         return front;
+// //     }
+
+// //     public static int peek(){
+// //         if(isEmpty()){
+// //             System.out.println("Already empty you cant delete anything ");
+// //             return -1;
+// //         }
+// //         return head.data;
+// //     }
+// // }
+
+// // public static void main(String args[]){
+// //     queue q =  new queue();
+// //     q.add(1); q.add(2); q.add(3); q.add(4); q.add(5);  q.add(6);
+
+// //     while(!q.isEmpty()){
+// //         System.out.println(q.peek());
+// //         q.delete();
+// //     }
+// // }
+
+// ////////  QUEUE USING COLLECTION FRAMEWORK //////////// ARRAYDEQUE n LINKLIST//
+
+// public static void main( String args[])
+// {
+//     Queue<Integer> q= new LinkedList<>();
+//     /*   //creates a new ll behaves like a queue n store it a var thst allows queue operations//
+//     QUEUE IS AN INTERFACE - A CONTRACT
+//                                 DEFINE WHAT METHOD EXIST,NOT HOW THEY WORK
+//     LL IS A CLASS -CONTRACTER
+//                        PROVIDES REAL IMPLEMETATION FOR THE QUEUE METHODS{add,remove}
+//     */
+    
+//     q.add(1);q.add(2);
+//     while(!q.isEmpty())
 //     {
-//         Node nd= new Node(data);
-
-//         if(tail == null)
-//         {
-//             head=tail=nd;
-//         }
-//         tail.next=nd;
-//         tail=nd;
-//     }
-//     public static int delete(){
-//         if(isEmpty()){
-//             System.out.println("Already empty you cant delete anything ");
-//             return -1;
-//         }
-//         int front=head.data;
-
-//         if(head== null)
-//         tail=null;
-
-//         head=head.next;
-//         return front;
-//     }
-
-//     public static int peek(){
-//         if(isEmpty()){
-//             System.out.println("Already empty you cant delete anything ");
-//             return -1;
-//         }
-//         return head.data;
-//     }
-// }
-
-// public static void main(String args[]){
-//     queue q =  new queue();
-//     q.add(1); q.add(2); q.add(3); q.add(4); q.add(5);  q.add(6);
-
-//     while(!q.isEmpty()){
 //         System.out.println(q.peek());
-//         q.delete();
+//         q.remove();
+
 //     }
+
+//     /*we can do the above same like
+//      * Queue<Integer> q = new ArrayDeque<>();
+//      * BOTH SAME o(1)
+//      * BUT WHEN WE SEARCH AN MIDDLE ELEMENT ARRAYDEQUE O(1) LL-O(n)
+//      */
+    
 // }
 
-////////  QUEUE USING COLLECTION FRAMEWORK //////////// ARRAYDEQUW LINKLIST//
+static class queue{
+    static int arr[];
+    static int size;
+    static int rear=-1; static int front=-1;
 
-public static void main( String args[])
-{
-    Queue<Integer> q= new LinkedList<>();
-    /*   //creates a new ll behaves like a queue n store it a var thst allows queue operations//
-    QUEUE IS AN INTERFACE - A CONTRACT
-                                DEFINE WHAT METHOD EXIST,NOT HOW THEY WORK
-    LL IS A CLASS -CONTRACTER
-                       PROVIDES REAL IMPLEMETATION FOR THE QUEUE METHODS{add,remove}
-    */
-    
-    q.add(1);q.add(2);
-    while(!q.isEmpty())
-    {
-        System.out.println(q.peek());
-        q.remove();
-
+    public static boolean isEmpty(){
+        return rear == -1 & front ==-1;
     }
-
-    /*we can do the above same like
-     * Queue<Integer> q = new ArrayDeque<>();
-     * BOTH SAME o(1)
-     * BUT WHEN WE SEARCH AN MIDDLE ELEMENT ARRAYDEQUE O(1) LL-O(n)
-     */
-    
+    public static boolean isFull(){
+        return  (rear+1)%size==front;
+    }
 }
 
-}
+ }
+
