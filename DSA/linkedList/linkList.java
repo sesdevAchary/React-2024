3e704ba68336a82210694c0ad3446866d99a4173
sesdevAchary <sesdev.achary@gmail.com>
2025-10-11 10:37:11 +0000

    reversing the itr

--- DSA/linkedList/linkList.java
+++ DSA/linkedList/linkList.java
@@ -1,6 +1,7 @@
 package DSA.linkedList;
 
 public class linkList {
+    Node head;
     class Node{
         int data;Node next;
         Node(int data){
@@ -10,23 +11,37 @@ public class linkList {
 
     public void reverseItr()
     {
-        Node head;
-        
         Node next;
         Node prev=null;
         Node current=head;
 
         while(current!=null){
             next=current.next;  // stores the next value
-            current.next=prev;
-            prev=current;
-            current=next;
-
-
+            current.next=prev;  // Now b → a instead of b → c. (reverse ptr)
+            prev=current;       // move prev forward
+            current=next;       // move current forward
         }
+        head=prev;
+    }
+
+    public void printList(){
+        Node current=head;
+
+        while(current != null)
+        {
+            System.out.print(current.data+"->");
+            current=current.next;
+        }
+         System.out.println("null");
+    }
+    public static void main ( String args[])
+    {
+        linkList l= new linkList();
+        l.reverseItr();
+        l.printList();
     }
 
         
-    }
+    
     
 }
