f973dc074020291e68d8fdcb9cda313c7493a562
sesdevAchary <sesdev.achary@gmail.com>
2025-10-13 15:35:23 +0000

    obtaining the main value

--- DSA/queueY/queueClass.java
+++ DSA/queueY/queueClass.java
@@ -225,6 +225,11 @@ public static void main(String args[])
 {
    Queue<Integer> q =     new LinkedList<>();
 
+   q.add(10);q.add(20);q.add(30);q.add(40);
+   System.out.println("Original Queue: " + q);
+        reverseQuese(q);
+        System.out.println("Reversed Queue: " + q);
+
 }
 
  }
